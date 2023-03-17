137e4ceacf872f0241084dec067673a14a16cbd0
Marte Rodriguez <marte.rod0712@gmail.com>
2023-03-17 12:59:07 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+export const posts = [
+    {
+        name: 'Red Ranger',
+        username: 'RedForever0708',
+        location: 'The GYM',
+        avatar: 'profile1.jpg',
+        post: 'post1.jpg',
+        comment: 'This guy looks evil! @Rangers lets get him and protect JUSTICE',
+        likes:  '1'
+    },
+    {
+        name: 'Pink Ranger',
+        username: 'PrettyINPink0921',
+        location: 'Hair Salon',
+        avatar: 'profile2.jpg',
+        post: 'post2.jpg',
+        comment: 'OMG, this girl is so FINE! Wanna hang out with me in the salon @NOTYellow0712',
+        likes: '244,244',
+    },
+    {
+        name: 'Black Ranger',
+        username: 'OYTB-YNGB123',
+        location: 'Somewhere Out There',
+        avatar: 'profile3.jpg',
+        post: 'post3.jpg',
+        comment: 'My man @bluesCLUES0825 you copy my tusks, just upside down and made it horns! :D',
+        likes: '34,555'
+    },
+]
---
+++ index.js
@@ -0,0 +1,77 @@
+import { posts } from "./data.js"
+
+const postEl = document.getElementById('post-el')
+
+for (let post of posts) {
+    postEl.innerHTML +=
+    `
+    <div class="container" >
+        <div class="user-container-image">
+            <img class="user-pic" src='./images/${post.avatar}' alt="Username profile picture">
+            <div>
+                <p class='user-name'>${post.name}</p>
+                <p clas='user-location'>${post.location}</p>
+            </div>
+        </div>
+    
+        <div class='user-post'>
+            <img class='user-pic-post' src='./images/${post.post}' alt='posted image of user'>
+        </div>
+
+        <div class='user-buttons'>
+            <img id='icon-like-btn' class='icons' id='heart-icon' src='./images/icon-heart.png' alt='heart icon for liking post'/>
+            <img class='icons' src='./images/icon-comment.png' alt='icon for commenting on posts' />
+            <img class='icons' src='./images/icon-dm.png' alt='icon for direct messaging user' />
+
+            <div class='user-likes'>
+                <p><span class='likes'>${post.likes}</span> like</p>
+            </div>
+        </div>
+
+        <div class='user-likes'>
+            <p id='likes'>${post.likes} likes</p>
+        </div>
+
+        <div class='user-comment'>
+            <p class='comment'>${post.comment}</p>
+        </div>
+
+    </div>
+    `
+
+    if (post.likes <= 1) {
+        postEl.innerHTML = 
+        `
+        <div class="container" >
+            <div class="user-container-image">
+                <img class="user-pic" src='./images/${post.avatar}' alt="Username profile picture">
+                <div>
+                    <p class='user-name'>${post.name}</p>
+                    <p clas='user-location'>${post.location}</p>
+                </div>
+            </div>
+        
+            <div class='user-post'>
+                <img class='user-pic-post' src='./images/${post.post}' alt='posted image of user'>
+            </div>
+
+            <div class='user-buttons'>
+                <img id='icon-like-btn' class='icons' id='heart-icon' src='./images/icon-heart.png' alt='heart icon for liking post'/>
+                <img class='icons' src='./images/icon-comment.png' alt='icon for commenting on posts' />
+                <img class='icons' src='./images/icon-dm.png' alt='icon for direct messaging user' />
+
+                <div class='user-likes'>
+                    <p><span class='likes'>${post.likes}</span> like</p>
+                </div>
+            </div>
+
+            <div class='user-comment'>
+                <p class='comment'>${post.comment}</p>
+            </div>
+
+        </div>
+        `
+    }
+}
+
+
